cc8507cba8f76a44e0e282bd55473d5770366129
Ian <ian.li@student.education.wa.edu.au>
2026-09-08 10:46:48 +0800

    stupid me made a dumb mistake

--- public/game.js
+++ public/game.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const fric = 0.5
 const accel = 0.5
-const speed = 0
+let speed = 0
 const maxSpeed = 5
 const jumpHeight = 10
 const grav = 0.5
@@ -49,11 +49,11 @@ function update() {
             players[myId].y += jumpHeight; moved = true;
         }
         if (keys.ArrowLeft || keys2.A)  {
-            let speed = Math.min(speed + accel, maxSpeed);
+            speed = Math.min(speed + accel, maxSpeed);
             players[myId].x -= speed; moved = true; side = true
         }
         if (keys.ArrowRight || keys2.D) {
-            let speed = Math.min(speed - accel, -1 * maxSpeed);
+            speed = Math.min(speed - accel, -1 * maxSpeed);
             players[myId].x -= speed; moved = true; side = true
         }
 
