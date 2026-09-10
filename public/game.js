abedf68244fb124931a25ab10b554ec73c5b5d05
Ian <ian.li@student.education.wa.edu.au>
2026-09-10 22:22:16 +0800

    very very rough probably buggy test

--- public/game.js
+++ public/game.js
@@ -3,14 +3,74 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const fric = 0.1
 const accel = 1
-let speed = 0
-let jump = 0
 const maxSpeed = 5
 const jumpHeight = 10
 const grav = 0.1
 
 let players = {};
 let myId = null;
+
+class col {
+    static checkAABB(a, b) {
+        return a.x < b.x + b.width &&
+               a.x + a.width > b.x &&
+               a.y < b.y + b.height &&
+               a.y + a.height > b.y;
+    }
+    
+}
+
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 0;
+    this.jump = 0;
+    this.hitbox = {
+      offsetX: 20,
+      offsetY: 40,
+      width: 24,
+      height: 24
+    };
+  }
+  getHitbox() {
+    return {
+      x: this.x + this.hitbox.offsetX,
+      y: this.y + this.hitbox.offsetY,
+      width: this.hitbox.width,
+      height: this.hitbox.height
+    };
+  }
+  update() {
+    let moved = false;
+    if (keys.ArrowUp || keys2.W || keys.Space) {
+        this.jump -= jumpHeight; moved = true;
+    }
+    if (keys.ArrowDown || keys2.S)  { 
+        //empty
+    }
+    if (keys.ArrowLeft || keys2.A)  {
+        this.speed = Math.min(this.speed + accel, maxSpeed);
+        moved = true;
+    }
+    if (keys.ArrowRight || keys2.D) {
+        this.speed = Math.max(this.speed - accel, -maxSpeed);
+        moved = true;
+    }
+    this.jump += grav;
+    if (this.speed > 0) {
+        this.speed = Math.max(this.speed - fric, 0);
+    }
+    else if (this.speed < 0) {
+        this.speed = Math.min(this.speed + fric, 0);
+    }
+    this.y += this.jump;
+    this.x -= this.speed; 
+    if (moved) {             
+        socket.emit('playerMovement', { x: this.x, y: this.y });         
+    }    
+  }
+}
 
 const keys = { ArrowUp: false, ArrowDown: false, ArrowLeft: false, ArrowRight: false, Space: false};
 const keys2 = { W: false, A: false, S: false, D: false};
@@ -27,7 +87,6 @@ window.addEventListener('keyup', (e) => {
     if (keyUpper in keys2) keys2[keyUpper] = false;
 });
 
-
 socket.on('connect', () => { myId = socket.id; });
 socket.on('currentPlayers', (serverPlayers) => { players = serverPlayers; });
 socket.on('newPlayer', (data) => { players[data.id] = data.player; });
@@ -41,39 +100,12 @@ socket.on('playerDisconnected', (id) => { delete players[id]; });
 
 function update() {
     if (myId && players[myId]) {
-        let moved = false;
-        if (keys.ArrowUp || keys2.W || keys.Space) {
-            jump -= jumpHeight; moved = true;
-        }
-        if (keys.ArrowDown || keys2.S)  { 
-            //empty
-        }
-        if (keys.ArrowLeft || keys2.A)  {
-            speed = Math.min(speed + accel, maxSpeed);
-            moved = true;
-        }
-        if (keys.ArrowRight || keys2.D) {
-            speed = Math.max(speed - accel, -maxSpeed);
-            moved = true;
-        }
-        jump += grav;
-        if (speed > 0) {
-            speed = Math.max(speed - fric, 0);
-        }
-        else if (speed < 0) {
-            speed = Math.min(speed + fric, 0);
-        }
-        players[myId].y += jump;
-        players[myId].x -= speed; 
-        if (moved) {             
-            socket.emit('playerMovement', { x: players[myId].x, y: players[myId].y });         
-        }    
+        players[myId].update();
     }
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let id in players) {
         ctx.fillStyle = players[id].color;
-        ctx.fillRect(players[id].x, players[id].y, 20, 20); // Draw player as a square
+        ctx.fillRect(players[id].x, players[id].y, 20, 20);
     }
 
     requestAnimationFrame(update);
